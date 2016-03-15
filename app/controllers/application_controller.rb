67de017f8499229c2d8316902e0d80fb2bdff348
jaimeh760 <jaimeh760@gmail.com>
2016-03-15 21:45:29 +0000

    Fwitter Dos!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ class ApplicationController < Sinatra::Base
     erb :index 
   end
 
+  get '/tweet' do
+    erb :tweet
+  end
+  
+  post '/tweet' do
+    # put params (puts into terminal, not on the preview)
+    # 'Hello from the Tweet Post Request!' 
+    # username = params[:username]
+    # status = params[:status]
+    # Tweet.new(username, status)
+    Tweet.new(params[:username], params[:status])
+    redirect '/'
+  end
+
 end
